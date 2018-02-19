0df658022c0c1259db4d1dd8452a90abe97490aa
zac <zschreck@iastate.edu>
2018-02-18 23:32:43 -0600

    stuff

---
+++ download_symbols.py
@@ -0,0 +1,53 @@
+from bs4 import BeautifulSoup
+import requests
+
+alphabet = ('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K','L', 'M',
+            'O', 'P', 'Q', 'R', 'S','T', 'U', 'V', 'W', 'X', 'Y', 'Z')
+exchanges = ('NYSE', 'NASDAQ')
+
+
+def put_symbols_in_file(symbols):
+    url = 'http://www.eoddata.com/stocklist/{0}/{1}.htm'
+    for exchange in exchanges:
+        for letter in alphabet:
+            page = requests.get(url.format(exchange, letter))
+            contents = page.content
+            soup = BeautifulSoup(contents, 'html.parser')
+            links = soup.find_all('a')
+            for soupy in links:
+                link = soupy.get('href')
+                if '/stockquote/{}/'.format(exchange) in link:
+                    if link[13 + len(exchange):-4] not in symbols:
+                        symbols.append(link[13 + len(exchange):-4])
+    text_file = open('symbols.txt', 'w+')
+
+    for symbol in symbols:
+        text_file.write(symbol + '|')
+    text_file.close()
+
+
+def load_symbols_from_file():
+    text_file = open('symbols.txt', 'r')
+    text = text_file.read()
+    symbols = filter(None, text.split('|'))
+    text_file.close()
+    return symbols
+
+
+def sort_symbols_file():
+    symbols = load_symbols_from_file()
+    open('symbols.txt', 'w').close() #clear the text file
+    symbols.sort()
+    text_file = open('symbols.txt', 'w+')
+    for symbol in symbols:
+        text_file.write(symbol + '|')
+    text_file.close()
+
+
+def main():
+    put_symbols_in_file(load_symbols_from_file())
+    sort_symbols_file()
+
+
+if __name__ == "__main__":
+    main()
---
+++ load_stats_in_csv.py
@@ -0,0 +1,38 @@
+import requests
+from download_symbols import load_symbols_from_file
+import json
+
+symbols = load_symbols_from_file()
+
+iex_api_prefix = 'https://api.iextrading.com/1.0'
+
+
+def load_stats_in_csv():
+    csv = prepare_csv()
+    symbols_string = ''
+    for n in range(len(symbols)):
+        symbols_string += symbols[n]
+        symbols_string += ','
+        if n % 100 == 0 and n > 0: # max of 100 stocks per api request
+            query_string = iex_api_prefix + '/stock/market/batch?symbols=' + symbols_string + '&types=quote'
+            response = requests.get(query_string).content
+            json_format = json.loads(response)
+            for key, value in json_format.iteritems():
+                value = value['quote']
+                csv.write("{0},{1},{2},{3},{4},{5},{6},{7},{8}\n".format(key, value['companyName'],
+                                                                         value['close'], value['sector'],
+                                                                         value['avgTotalVolume'], value['marketCap'],
+                                                                         value['peRatio'], value['week52High'],
+                                                                         value['week52Low']))
+            symbols_string = ''
+
+
+def prepare_csv():
+    open("cashmoney.csv", "w").close()
+    csv = open("cashmoney.csv", "w")
+    csv.write("symbol,company,price,sector,avg_volume,market_cap,pe_ratio,week52high,week52low\n")
+    return csv
+
+
+if __name__ == '__main__':
+    load_stats_in_csv()
